b883953de913192898ec51053822b0f4f65a0f29
Lior Degu <lior.degu@gmail.com>
2024-06-08 22:27:44 +0300

    update requirements

--- main.py
+++ main.py
@@ -9,31 +9,31 @@ def download_url(video_title, video_url):
 
     # Creating GET request
     r = requests.get(video_url, stream=True)
-    
+
     # Total size of downloaded video
     total_size = int(r.headers.get("content-length"))
 
     # Downloading video in 1MB chunks (Progress bar included)
     with open(video_title + ".mp4", "wb") as video:
-        for chunk in tqdm(iterable=r.iter_content(chunk_size=1024**2), 
-                          total=int(total_size/(1024**2)), 
+        for chunk in tqdm(iterable=r.iter_content(chunk_size=1024**2),
+                          total=int(total_size/(1024**2)),
                           ncols=100,
                           unit="MB"):
             if chunk:
                 video.write(chunk)
 
-                
+
 def main():
-    
+
     # Creating GET request
-    url = "RSS URL Here"
+    url = "https://{RSS URL Here}"
     r = requests.get(url)
-    
-    # Scraping using BeautifulSoup4 then parsing using lxml 
-    soup = bs(r.text, "lxml")
+
+    # Scraping using BeautifulSoup4 then parsing using lxml
+    soup = bs(r.text, features="xml")
 
     # Removing course title prefix
-    course_title = soup.find("title").text[9:] 
+    course_title = soup.find("title").text[9:]
 
     # Acquiring videos titles and urls
     content = soup.find_all("item")
@@ -48,6 +48,6 @@ def main():
     with concurrent.futures.ThreadPoolExecutor() as executor:
         executor.map(download_url, titles, urls)
 
-        
+
 if __name__ == "__main__":
     main()
